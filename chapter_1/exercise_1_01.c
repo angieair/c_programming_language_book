60d6e0709cef8a4a9cae0814685637b0fc78eaf5
Angela Chung <angela.chung1@gmail.com>
2018-04-29 11:54:56 -0700

    add comments

--- chapter_1/exercise_1_01.c
+++ chapter_1/exercise_1_01.c
@@ -1,6 +1,14 @@
+/* 
+	Exercise 1-1 
+	Run the "hello, world" program on your system. Experiment with leaving out parts of the program,
+	to see what error messages you get.
+*/
+
 #include <stdio.h>
 
 main()
 {
 	printf("hello, world\n");
+	
+    return 0;
 }
